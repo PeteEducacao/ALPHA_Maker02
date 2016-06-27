70a619e079cdbdc71809dc775772d3b6f6a6d951
Jpap666 <pacheco@pete.com.br>
2016-06-27 15:10:12 -0300

    Update MakerX2.js

--- MakerX2.js
+++ MakerX2.js
@@ -782,8 +782,7 @@
 		['-'],
 		['w', 'Tocar nota %m.notes por %n segundos', 'playNoteTime', menus['notes'][0], 1],
 		[' ', 'Tocar nota %m.notes', 'playNote', menus['notes'][0]],
-		[' ', 'Mudo', 'mute'],
-		[' ', 'Siga Luz', 'sigaLuz']
+		[' ', 'Mudo', 'mute']
 	];
 	
 	var descriptor = {
