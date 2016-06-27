a17a0bef855aa1eda6c0e0ddedc8d487b2077d1f
Jpap666 <pacheco@pete.com.br>
2016-06-27 11:58:04 -0300

    Update MakerX2.js

--- MakerX2.js
+++ MakerX2.js
@@ -609,7 +609,7 @@
 					resend[2] = 13; //\r
 					if(device)
 						device.send(resend.buffer);
-				}, 100);
+				}, 50);
 			
 				//Set a timer to check if the connection is still active
 				active = true;
